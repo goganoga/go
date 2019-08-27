bc489a9f5ac037229216478a08ebcbaa0872e92b
AntonD <daa10599160@gmail.com>
2019-08-27 23:23:27 +0300

    multithread

--- main.go
+++ main.go
@@ -7,18 +7,29 @@ import (
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"math/rand"
 	"net/http"
 	"net/url"
+	"os"
+	"os/signal"
 	"strconv"
+	"syscall"
 	"time"
 
+	mux "github.com/gorilla/mux"
 	uuid "github.com/satori/go.uuid"
 )
 
+type httpService struct {
+	server *http.Server
+}
+
 type jsonUser struct {
 	Status        int    `json:"status"`
-	Lock          string `json:"lock"`
+	Uri           string `json:"uri"`
+	Location      string `json:"location"`
+	PostBody      string `json:"postBody"`
 	Param         string `json:"param[qq]"`
 	Hash_sha256   string `json:"hash_sha256"`
 	Hash_md5      string `json:"hash_md5"`
@@ -28,7 +39,6 @@ type jsonUser struct {
 	H_sha512_gen  string `json:"h_sha512_gen"`
 	Hash_h        string `json:"hash_h"`
 }
-type msg string
 
 var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
@@ -41,7 +51,7 @@ func RandStringRunes() string {
 	}
 	return string(b)
 }
-func (msg) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
+func (this *httpService) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
 	var param string = "null"
 	buf := string(req.URL.String())
 	params, _ := url.Parse(buf)
@@ -49,6 +59,7 @@ func (msg) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
 	if params_ != "" {
 		param = params_
 	}
+	PostBody, _ := ioutil.ReadAll(req.Body)
 	Hash_sha256 := sha256.Sum256([]byte(buf))
 	Hash_md5 := md5.Sum([]byte(buf))
 	Guid, _ := uuid.NewV4()
@@ -58,7 +69,9 @@ func (msg) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
 	Hash_h := sha256.Sum256([]byte(buf + hex.EncodeToString(Hash_md5[:]) + strconv.FormatInt(time.Now().Unix(), 10)))
 	jsonA := &jsonUser{
 		Status:        1,
-		Lock:          buf,
+		Uri:           buf,
+		Location:      req.URL.EscapedPath(),
+		PostBody:      string(PostBody),
 		Param:         param,
 		Hash_sha256:   hex.EncodeToString(Hash_sha256[:]),
 		Hash_md5:      hex.EncodeToString(Hash_md5[:]),
@@ -70,8 +83,33 @@ func (msg) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
 	jsonB, _ := json.Marshal(jsonA)
 	fmt.Fprint(resp, string(jsonB))
 }
+func (this *httpService) Start() {
+	if this.server != nil {
+		panic("Server already started.")
+	}
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/test", this.ServeHTTP).Methods("GET", "POST")
+
+	this.server = &http.Server{
+		Addr:    "localhost:10800",
+		Handler: router,
+	}
+
+	go func() {
+		if err := this.server.ListenAndServe(); err != nil {
+			panic("Failed to listen.")
+		}
+	}()
+}
 func main() {
-	msgHandler := msg("L")
+	msgHandler := httpService{}
 	fmt.Println("Server is listening...")
-	http.ListenAndServe("localhost:10800", msgHandler)
+	msgHandler.Start()
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	fmt.Println("Press Ctrl+C for quit.")
+
+	<-c
 }
